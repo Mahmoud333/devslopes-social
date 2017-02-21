d39556ef41efbe4e95662c092a964afef8a4803a
Mahmoud333 <Mahmoud@mahmouds-mini.zte.com.cn>
2017-02-21 02:42:28 +0200

    Initial Commit

---
+++ devlslopes-Social Media (Udemy)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  devlslopes-Social Media (Udemy)
+//
+//  Created by Mahmoud Hamad on 2/21/17.
+//  Copyright © 2017 Mahmoud SMGL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ devlslopes-Social Media (Udemy)Tests/devlslopes_Social_Media__Udemy_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  devlslopes_Social_Media__Udemy_Tests.swift
+//  devlslopes-Social Media (Udemy)Tests
+//
+//  Created by Mahmoud Hamad on 2/21/17.
+//  Copyright © 2017 Mahmoud SMGL. All rights reserved.
+//
+
+import XCTest
+@testable import devlslopes_Social_Media__Udemy_
+
+class devlslopes_Social_Media__Udemy_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
