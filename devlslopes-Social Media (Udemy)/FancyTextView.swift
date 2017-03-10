a726f10f70a870639908dfd93fd41ea1aec2b448
Mahmoud333 <Mahmoud@mahmouds-mini.zte.com.cn>
2017-03-11 01:30:03 +0200

    finished first popost part

---
+++ devlslopes-Social Media (Udemy)/FancyTextView.swift
@@ -0,0 +1,21 @@
+//
+//  FancyTextView.swift
+//  devlslopes-Social Media (Udemy)
+//
+//  Created by Mahmoud Hamad on 3/10/17.
+//  Copyright © 2017 Mahmoud SMGL. All rights reserved.
+//
+
+import UIKit
+
+class FancyTextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
